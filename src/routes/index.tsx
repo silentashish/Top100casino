09cab243386288b6d08410238ab7b2ab9ab97150
Ashish Gautam <ashish.gm74@gmail.com>
2021-05-03 21:51:58 +0545

    saved state

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,51 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {StackRoute} from './StackRoute';
 import Toast from 'react-native-toast-message';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Linking, Platform} from 'react-native';
+
+const PERSISTENCE_KEY = 'NAVIGATION_STATE';
 
 export default () => {
+  const [isReady, setIsReady] = React.useState(false);
+  const [initialState, setInitialState] = React.useState();
+
+  React.useEffect(() => {
+    const restoreState = async () => {
+      try {
+        const initialUrl = await Linking.getInitialURL();
+
+        if (Platform.OS !== 'web' && initialUrl == null) {
+          // Only restore state if there's no deep link and we're not on web
+          const savedStateString = await AsyncStorage.getItem(PERSISTENCE_KEY);
+          const state = savedStateString
+            ? JSON.parse(savedStateString)
+            : undefined;
+
+          if (state !== undefined) {
+            setInitialState(state);
+          }
+        }
+      } finally {
+        setIsReady(true);
+      }
+    };
+
+    if (!isReady) {
+      restoreState();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      initialState={initialState}
+      onStateChange={state =>
+        AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state))
+      }>
       <StackRoute />
       <Toast ref={ref => Toast.setRef(ref)} />
     </NavigationContainer>
